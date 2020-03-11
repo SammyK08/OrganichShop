24fb20ca9e1555ecf7be5e2072b9b42b7e88e5a9
samraj karki <samrajkarki08@gmail.com>
2020-03-11 02:32:03 -0400

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+// 1. Import the libs you need
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+import { AppComponent } from "./app.component";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDrrLz5la16L53cfA_go94JOwiZkYh7fI",
+  authDomain: "oshop-7bc83.firebaseapp.com",
+  databaseURL: "https://oshop-7bc83.firebaseio.com",
+  projectId: "oshop-7bc83",
+  storageBucket: "oshop-7bc83.appspot.com",
+  messagingSenderId: "910580078056",
+  appId: "1:910580078056:web:9145d9aa28e48abf35c05d",
+  measurementId: "G-4N21NRHKT3"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // 3. Initialize
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
