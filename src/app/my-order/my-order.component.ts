ebebc038f7c40ffc7d6b3ec2ad5478d8bdf7482c
samraj karki <samrajkarki08@gmail.com>
2020-03-11 08:29:07 -0400

    adding dropdown to menu

--- src/app/my-order/my-order.component.ts
+++ src/app/my-order/my-order.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-my-order',
-  templateUrl: './my-order.component.html',
-  styleUrls: ['./my-order.component.css']
+  selector: "app-my-order",
+  templateUrl: "./my-order.component.html",
+  styleUrls: ["./my-order.component.css"]
 })
 export class MyOrderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
